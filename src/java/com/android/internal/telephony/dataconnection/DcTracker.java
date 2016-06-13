8870846426a7dd19763378432ce85bedefcc7960
Yashdev Singh <yashdevs@codeaurora.org>
2016-06-13 14:39:45 -0700

    Telephony: Enable NV based CDMA data call.
    Change-Id: Ibe9eed3c7e2062e8ea444d0e0de7a1f77cbe85ec
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -1238,6 +1238,11 @@ public class DcTracker extends Handler {
         return mAttached.get();
     }
 
+    private boolean isNvSubscription() {
+        int cdmaSubscriptionSource = CdmaSubscriptionSourceManager.getDefault(mPhone.getContext());
+        return cdmaSubscriptionSource == CdmaSubscriptionSourceManager.SUBSCRIPTION_FROM_NV;
+    }
+
     /**
      * Check if it is allowed to make a data connection (without checking APN context specific
      * conditions).
